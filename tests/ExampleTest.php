ba906d8947d83390ed32bfed02c58539b68db317
Renato Gomes <renatoo.agomes@gmail.com>
2016-05-15 18:23:13 -0300

    Commit com selenium server, ExampleTest usando do Selenium

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,8 +4,12 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+use Laracasts\Integrated\Extensions\Selenium;
+
+class ExampleTest extends Selenium
 {
+    protected $baseUrl = "http://localhost:8000/";
+
     /**
      * A basic functional test example.
      *
